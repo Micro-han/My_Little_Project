87ce3bd6ca79dafe57d4bf273b824c2ea0ca1e83
Xiaohan Yan <44750451+Micro-han@users.noreply.github.com>
2022-10-27 20:08:41 +0800

    main

---
+++ cats_vs_dogs/getdata.py
@@ -0,0 +1,62 @@
+import os
+import torch.utils.data as data
+from PIL import Image
+import torch
+import torchvision.transforms as transforms
+
+IMAGE_SIZE = 200
+
+dataTransform = transforms.Compose([
+    transforms.Resize((IMAGE_SIZE, IMAGE_SIZE)),
+    transforms.CenterCrop((IMAGE_SIZE, IMAGE_SIZE)),
+    transforms.ToTensor()
+])
+
+
+class PetsDataset(data.Dataset):
+    def __init__(self, mode, path):
+        self.mode = mode
+        self.path = path
+        self.list_img = []
+        self.list_label = []
+        self.data_size = 0
+        self.transform = dataTransform
+        self.cat_dir = ''
+        self.dog_dir = ''
+        # /archive/PetImages/Cat
+        # /archive/PetImage/Dog
+        if self.mode == 'train':
+            self.cat_dir = self.path + '/Cat/'
+            self.dog_dir = self.path + '/Dog/'
+            for file in os.listdir(self.cat_dir):
+                if file == '.DS_Store':
+                    continue
+                self.list_img.append(self.cat_dir + file)
+                self.data_size += 1
+                self.list_label.append(0)
+            for file in os.listdir(self.dog_dir):
+                if file == '.DS_Store':
+                    continue
+                self.list_img.append(self.dog_dir + file)
+                self.data_size += 1
+                self.list_label.append(1)
+        elif self.mode == 'test':
+            self.test_dir = self.path
+            for file in os.listdir(self.test_dir):
+                if file == '.DS_Store':
+                    continue
+                self.list_img.append(self.test_dir + file)
+                self.data_size += 1
+                self.list_label.append(-1)
+
+    def __getitem__(self, item):
+        if self.mode == 'train':
+            img = Image.open(self.list_img[item]).resize((IMAGE_SIZE, IMAGE_SIZE)).convert('RGB')
+            label = self.list_label[item]
+            return self.transform(img), torch.LongTensor([label])
+        elif self.mode == 'test':
+            img = Image.open(self.list_img[item]).resize((IMAGE_SIZE, IMAGE_SIZE)).convert('RGB')
+            return self.transform(img)
+
+    def __len__(self):
+        return self.data_size
---
+++ cats_vs_dogs/main.py
@@ -0,0 +1,90 @@
+import os
+from PIL import Image
+import matplotlib.pyplot as plt
+import getdata
+
+from getdata import PetsDataset as PetsDataset
+from torch.utils.data import DataLoader as DataLoader
+from network import Net
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+dataset_dir = './archive/PetImages'
+model_cp = './model/'
+workers = 10
+batch_size = 16
+lr = 0.0001
+n_epoch = 10
+model_file = './model/model.pth'
+test_dir = './test/'
+
+
+def train():
+    data_file = PetsDataset('train', dataset_dir)
+    data_loader = DataLoader(data_file, batch_size=batch_size, shuffle=True, num_workers=0, drop_last=True)
+
+    model = Net()
+    if torch.cuda.is_available():
+        model = model.cuda()
+    model = nn.DataParallel(model)
+    model.train()
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+    criterion = torch.nn.CrossEntropyLoss()
+    cnt = 0
+    for epoch in range(n_epoch):
+        for img, label in data_loader:
+            if torch.cuda.is_available():
+                img, label = Variable(img).cuda(), Variable(label).cuda()
+            else:
+                img, label = Variable(img).cpu(), Variable(label).cpu()
+            out = model(img)
+            loss = criterion(out, label.squeeze())
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+            cnt += 1
+            print('Epoch: {0}, Frame: {1}, train_loss: {2}'.format(epoch, cnt * batch_size, loss / batch_size))
+    torch.save(model.state_dict(), '{0}/model.pth'.format(model_cp))
+
+
+def test():
+    model = Net()
+    if torch.cuda.is_available():
+        model = model.cuda()
+    model = nn.DataParallel(model)
+    model.load_state_dict(torch.load(model_file))
+    model.eval()
+    files = [file for file in os.listdir(test_dir)]
+    imgs = []
+    imgs_data = []
+    for file in files:
+        img = Image.open(test_dir + file)
+        img_data = getdata.dataTransform(img)
+
+        imgs.append(img)
+        imgs_data.append(img_data)
+
+    out = model(imgs_data)
+    out = F.softmax(out, dim=1)
+    out = out.data.cpu().numpy()
+
+    for idx in range(len(files)):
+        print(files[idx], out[idx, 0], out[idx, 1])
+        plt.figure()
+        if out[idx, 0] > out[idx, 1]:
+            plt.suptitle('cat: {:.1%}, dog: {:.1%}'.format(out[idx, 0], out[idx, 1]))
+        else:
+            plt.suptitle('dog: {:.1%}, cat: {:.1%}'.format(out[idx, 1], out[idx, 0]))
+        plt.imshow(imgs[idx])
+    plt.show()
+
+
+if __name__ == '__main__':
+    mode = input()
+    if mode == 'train':
+        train()
+    else:
+        test()
